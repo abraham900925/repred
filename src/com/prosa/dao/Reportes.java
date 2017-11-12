3f15fdf29e886df0b6dcc0a440dadd4c6fb1dc6e
abraham <abraham900925@gmail.com>
2017-11-11 19:16:25 -0600

    se agregan clases

--- src/com/prosa/dao/Reportes.java
+++ src/com/prosa/dao/Reportes.java
@@ -1,6 +1,12 @@
 package com.prosa.dao;
 
 
+import com.prosa.dto.Stat06ntDto;
+import org.apache.poi.ss.usermodel.Workbook;
+import java.sql.*;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Sheet;
+import java.util.List;
 import org.apache.poi.ss.usermodel.Workbook;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,7 +20,8 @@ import org.apache.poi.ss.usermodel.Workbook;
  */
 public  interface Reportes 
 {
+    public void buildReport(String query);
+    public String buildTableReport(ResultSet rs);
     
-    public Workbook buildReport();
     
 }
